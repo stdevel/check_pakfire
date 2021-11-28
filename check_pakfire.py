dc38bed54332ec8154761247f2e6de87f11e28d0
Christian Stankowic <info@cstan.io>
2021-11-28 11:05:44 +0100

    strictly set Python 3 in Shebang

--- check_pakfire.py
+++ check_pakfire.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 A Nagios/Icinga plugin for checking a IPFire host
-# for available Pakfire updates
+for available Pakfire updates
 """
 
 import argparse
